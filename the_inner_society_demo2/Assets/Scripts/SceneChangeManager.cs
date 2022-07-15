c8ecc09cef009d44fd008077557f32348b61ed30
Steffen Kahl <stevenpaw209@gmail.com>
2022-07-15 10:16:32 +0200

    [CHANGED] Fixed SceneLayers

---
+++ the_inner_society_demo2/Assets/Scripts/SceneChangeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace farmingsim
+{
+    public static class SceneChangeManager
+    {
+        private static Dictionary<string, Scene> scenes;
+
+        public static Dictionary<string, Scene> Scenes
+        {
+            get => scenes;
+            set => scenes = value;
+        }
+
+        public static bool ChangeScene(string newScene)
+        {
+            if(SceneMan)
+        }
+    }
+}
